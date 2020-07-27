7bf53376bbd576a46c0c556f2735a71e47014d98
jplessey <jlessey@gmail.com>
2020-07-27 15:25:52 -0400

    Deploy microservice with kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="jlessey/microservice:latest"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run microservicedemo\
+    --image=$dockerpath\
+    --port=80 --labels="app=microservicedemo"
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward microservicedemo 8000:80
